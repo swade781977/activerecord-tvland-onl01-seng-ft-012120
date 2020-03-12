6e1976c2080939d98a94c1188c8fafab52872b5b
Chris Wade <swade781977@yahoo.com>
2020-03-12 16:28:24 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -9,9 +9,10 @@ class Show < ActiveRecord::Base
     a = self.actors
     first_name = a[0].first_name
     last_name = a[0].last_name
-    arr = [first_name, last_name]
-    arr.join(' ')
-    arr
+    format_arr = [first_name, last_name]
+    a = format_arr.join(' ')
+    arr = []
+    arr << a
     binding.pry
   end
 end
