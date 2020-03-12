7d2334f7a6caf63e26fb747abda01d6876e79751
Chris Wade <swade781977@yahoo.com>
2020-03-12 12:03:56 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
+require 'pry'
 class Show < ActiveRecord::Base
+  belongs_to :networks
+  has_many :characters
+  has_many :actors, through: :characters
   
+  def actors_list
+    c = self.characters
+    a = self.actors
+    first_name = a[0].first_name
+    last_name = a[0].last_name
+    arr = [first_name, last_name]
+    arr.join(' ')
+  end
 end
